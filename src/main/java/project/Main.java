14d14db4ef59a4beb13c610a16ec7bffa8809aaa
Daniel Ullrich <ullrichd@aldenv174.allegheny.edu>
2018-11-05 09:28:08 -0500

    First Work

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,7 +1,38 @@
 package project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String [] args) {
+    System.out.println("Allegheny Trail - Choose Your Adventure");
+    System.out.println("\nPlease choose an option:");
+
+    Scanner scanner = null;
+    try {
+      File saveFile = new File("saves/save.txt");
+      scanner = new Scanner(saveFile);
+      System.out.println("  -Continue");
+    } catch (FileNotFoundException noFile) {
+      System.out.println("---No Save File Found---");
+    }
+
+    System.out.println("  -Exit");
+
+
+    
+    /**
+    Scan all folders to see if there are more than one adventure? Maybe?
+
+    Scanner scanner = null;
+    try {
+      File gameOptions[] = new File("adventures/_/config.txt");
+      scanner = new Scanner(gameOptions[0]);
+    } catch (FileNotFoundException noFile) {
+      System.out.println("Unable to locate file");
+    }
+    */
 
   }
 }
