36c2bac57431d393acfe8e1fe0cf2423e1495008
Zachary Canali <canaliz@aldenv177.allegheny.edu>
2018-11-16 09:43:06 -0500

    Scene Actions update.

--- src/main/java/project/SceneActions.java
+++ src/main/java/project/SceneActions.java
@@ -15,11 +15,52 @@ public class SceneActions {
     String userInput = null;
     boolean inputOk = false;
     Scanner scan = new Scanner(System.in);
-    clearScreen(); //Flush Terminal
 
-    while (inputOk == false) {
-      System.out.println("\n");
+    userInput = scan.nextLine();
+    userInput = userInput.toLowerCase();
+    userInput = userInput.replace(" ", "");
 
-    }
+    System.out.println(userInput);
+
+    Scene sc = new Scene();
+    sc.readScene("00");
+
+    if (userInput != null) {
+      switch (userInput) {
+        case "sky":
+            //Examines the sky, this decision leads nowhere.
+            System.out.println("You look at the sky. It's twilight, and the stars
+            are starting to come out. It's beautiful. You acheive nothing by doing this.");
+            inputOk = true;
+            break;
+
+        case "ground":
+            //Examines the ground, which uncovers a path. This is the only option that works.
+            System.out.println("Successful path. You WIN. This is a placeholder.");
+            inputOk = true;
+            break;
+
+        case "trees":
+            //Examines the trees. Does nothing, but provide a bad pun.
+            System.out.println("Tree-ditionally the trees would not interest you.
+            However, there seem to be a Tree-lion, tree-angular, trees, all standing
+            like sen-trees. Its a beautiful coun-tree. \n\n\n I'm not sorry. \n You
+            were the one who chose to look at trees.");
+            inputOk = true;
+            break;
+
+        case "exit":
+            System.out.println("There is no escape.");
+            inputOk = true;
+            System.out.println("Exiting...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            break;
+        default:
+            System.out.println("Input Not Recognized!");
+            break;
   }
 }
